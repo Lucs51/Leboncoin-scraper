6c1db85583ffaa8db37e3ef879d62be51ba6bae9
Lucas Girard <32383976+Lucs51@users.noreply.github.com>
2024-02-11 00:43:26 +0100

    Add files via upload

---
+++ lbcscraper/items.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+from scrapy import Item, Field
+
+class LbcItem(Item):
+    title = Field()
+    link = Field()
+    city = Field()
+    postcode = Field()
+    price = Field()
+    photo = Field()
+    ges = Field()
+    energy_class = Field()
+
+class LbcPropertyItem(LbcItem):
+    surface_area = Field()
+
+class LbcCarItem(LbcItem):
+    make = Field()
+    model = Field()
+    year = Field()
+    mileage = Field()
+    fuel = Field()
+    gearbox = Field()
